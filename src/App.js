ebb48becfdd6c933f3691419f94a9717ed401886
carlos-fernandez303 <70593246+carlos-fernandez303@users.noreply.github.com>
2021-07-18 21:23:37 -0400

    Pushing banner

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import React from 'react';
 import './App.css';
 import Row from './Row.js'
 import requests from './requests.js'
-
+import Banner from './Banner.js'
 function App() {
   return (
-    <div className="App">
-      <h1> Yooo</h1>
-      <Row title="NETFLIX ORIGINALS" fetchURL={requests.fetchNetflixOriginals} />
+    <div className="app">
+      <Banner/>
+      <Row 
+        title="Netflix Originals" 
+        fetchURL={requests.fetchNetflixOriginals}
+        isLargeRow={true}
+      />
       <Row title="Trending Now" fetchURL={requests.fetchTrending}/>
+      <Row title="Top Rated" fetchURL={requests.fetchTopRated}/>
+      <Row title="Action Movies" fetchURL={requests.fetchActionMovies}/>
+      <Row title="Comedy Movies" fetchURL={requests.fetchComedyMovies}/>
+      <Row title="Horror Movies" fetchURL={requests.fetchHorrorMovies}/>
+      <Row title="Romance Movies" fetchURL={requests.fetchRomanceMovies}/>
+      <Row title="Documentaries" fetchURL={requests.fetchDocumentaries}/>
     </div>
   );
 }
--- src/Row.js
+++ src/Row.js
@@ -4,7 +4,7 @@ import './Row.css';
 
 const base_url = "https://image.tmdb.org/t/p/original/"
 
-function Row({ title, fetchURL }) {
+function Row({ title, fetchURL, isLargeRow }) {
     const [movies, setMovies] = useState([]);
 
 
@@ -30,8 +30,8 @@ function Row({ title, fetchURL }) {
                 {movies.map(movie => (
                     <img 
                     key={movie.id}
-                    className="row__poster"
-                    src={`${base_url}${movie.poster_path}`} alt={movie.name}
+                    className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+                    src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie.name}
                     alt={movie.name}
                     />
                 ))}

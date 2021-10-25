116bd4828e657f234258bab1df9a218e4dbb8460
kcheung3102 <kcheung3102@gmail.com>
2021-10-25 16:11:52 -0400

    allowed access to profile popup if user is logged in

--- src/components/Menu/MenuDropDown.js
+++ src/components/Menu/MenuDropDown.js
@@ -1,14 +1,26 @@
-import React, { useState} from 'react'
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
+import React, { useState } from "react";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
 import IconButton from "@mui/material/IconButton";
 import { useAuth0 } from "@auth0/auth0-react";
-import LoginButton from '../LoginButton/LoginButton';
-import LogOutButton from '../LogoutButton/LogoutButton';
+import LoginButton from "../LoginButton/LoginButton";
+import LogOutButton from "../LogoutButton/LogoutButton";
+import Profile from "../Profile/Profile.js";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import Button from "@mui/material/Button";
+import Slide from "@mui/material/Slide";
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
 
 function MenuDropDown() {
-  const [anchorEl, setAnchorEl] = useState(null)
-  const { loginWithRedirect ,isAuthenticated } = useAuth0();
+  const [anchorEl, setAnchorEl] = useState(null);
+  const { loginWithRedirect, isAuthenticated } = useAuth0();
   const open = Boolean(anchorEl);
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
@@ -20,45 +32,52 @@ function MenuDropDown() {
   const handleMenu = (event) => {
     setAnchorEl(event.currentTarget);
   };
-    return (
-        <div>
-         <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleMenu}
-              color="inherit"
-            >
-              <img
-                className="profile__logo"
-                src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
-                alt="Profile Logo"
-              ></img>
-            </IconButton>
+  return (
+    <div>
+      <IconButton
+        size="large"
+        aria-label="account of current user"
+        aria-controls="menu-appbar"
+        aria-haspopup="true"
+        onClick={handleMenu}
+        color="inherit"
+      >
+        <img
+          className="profile__logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+          alt="Profile Logo"
+        ></img>
+      </IconButton>
       <Menu
         id="basic-menu"
         anchorEl={anchorEl}
         open={open}
         onClose={handleClose}
         anchorOrigin={{
-          vertical: 'top',
-          horizontal: 'right',
+          vertical: "top",
+          horizontal: "right",
         }}
         transformOrigin={{
-          vertical: 'top',
-          horizontal: 'right',
+          vertical: "top",
+          horizontal: "right",
         }}
       >
-        <MenuItem onClick={handleClose}>Profile</MenuItem>
-        <MenuItem onClick={handleClose}>My account</MenuItem>
-        { isAuthenticated ? <LogOutButton /> : <LoginButton />}
-              <MenuItem onClick={() => loginWithRedirect({
-                  screen_hint: 'signup',
-              })}>Sign Up</MenuItem> 
+        {isAuthenticated ? <Profile /> : null}
+        <MenuItem>Settings</MenuItem>
+        {isAuthenticated ? <LogOutButton /> : <LoginButton />}
+        {isAuthenticated ? null : <MenuItem
+            onClick={() =>
+              loginWithRedirect({
+                screen_hint: "signup",
+              })
+            }
+          >
+            Sign Up
+          </MenuItem>
+          }
       </Menu>
-        </div>
-    )
+    </div>
+  );
 }
 
 export default MenuDropDown;
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,20 +1,58 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { useAuth0 } from "@auth0/auth0-react";
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Slide from '@mui/material/Slide';
+import MenuItem from '@mui/material/MenuItem';
 
-export const Profile = () => {
-    const { user, isAuthenticated, isLoading } = useAuth0();
-    
-    if (isLoading) {
-        return <div>Loading ...</div>;
-      }
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+const Profile = () => {
+    const { user, isAuthenticated } = useAuth0();
+    const [openDialog, setOpenDialog] = useState(false);
+
+    const handleClickOpenDialog = () => {
+      setOpenDialog(true);
+    };
+  
+    const handleCloseDialog = () => {
+      setOpenDialog(false);
+    }
+
     
       return (
         isAuthenticated && (
           <div>
-            <img src={user.picture} alt={user.name} />
-            <h2>{user.name}</h2>
-            <p>{user.email}</p>
+            <MenuItem onClick={handleClickOpenDialog}>Profile</MenuItem>
+        <Dialog
+        open={openDialog}
+        TransitionComponent={Transition}
+        keepMounted
+        onClose={handleCloseDialog}
+        aria-describedby="alert-dialog-slide-description"
+      >
+        <DialogTitle>{"Profile Information"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-slide-description">
+          <img src={user?.picture} alt={user?.name} />
+          <h2>Username: {user?.name}</h2>
+          <p>Email: {user?.email}</p>
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseDialog}>Close</Button>
+        </DialogActions>
+      </Dialog>
           </div>
         )
       );
 }
+
+export default Profile;
